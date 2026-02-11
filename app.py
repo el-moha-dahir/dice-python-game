400e1f082bf716c20c826ac99973db028f9f25ca
muniir <mohammedahir96@gmail.com>
2026-02-11 15:11:29 +0300

    dice

---
+++ app.py
@@ -0,0 +1,16 @@
+import random 
+
+while True:
+    choice = input("Roll the dice? (y/n): ").lower()
+    
+    if choice == 'y':
+        diel1 = random.randint(1, 6)
+        diel2 = random.randint(1, 6)
+        print(f"({diel1}, {diel2})")   # f-string to print variables
+        
+    elif choice == 'n':
+        print("Thanks for playing!")
+        break
+        
+    else:
+        print("Invalid choice!")
